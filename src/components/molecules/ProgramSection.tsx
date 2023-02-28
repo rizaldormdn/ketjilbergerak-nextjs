3d6dde643d26ba94d27007ed7b05ce2628974193
faizkur <mfaizk26@gmail.com>
2023-02-28 20:12:37 +0700

    update terbaru 28/02

--- src/components/molecules/ProgramSection.tsx
+++ src/components/molecules/ProgramSection.tsx
@@ -10,29 +10,34 @@ export const ProgramSection = () => {
             desc: "Sekolah Pemuda Desa (SPD) akan hadir kembali nih,tapi dengan format yang sedikit berbeda dari biasanya.Setelah 4 kali penyelenggaraan di DIY dan Jawa Tengah,akhir tahun ini SPD akan diselenggarakan di 3 kabupaten di 3 provinsi sekaligus."
         },
         {
-            title: "Sekolah Pemuda Desa",
+            title: "Video Production with BPIP",
             desc: "Sekolah Pemuda Desa (SPD) akan hadir kembali nih,tapi dengan format yang sedikit berbeda dari biasanya.Setelah 4 kali penyelenggaraan di DIY dan Jawa Tengah,akhir tahun ini SPD akan diselenggarakan di 3 kabupaten di 3 provinsi sekaligus."
         },
         {
-            title: "Sekolah Pemuda Desa",
+            title: "Ketjil Bergerak Music",
             desc: "Sekolah Pemuda Desa (SPD) akan hadir kembali nih,tapi dengan format yang sedikit berbeda dari biasanya.Setelah 4 kali penyelenggaraan di DIY dan Jawa Tengah,akhir tahun ini SPD akan diselenggarakan di 3 kabupaten di 3 provinsi sekaligus."
         },
         {
-            title: "Sekolah Pemuda Desa",
+            title: "Ketjil Bergerak Mural",
             desc: "Sekolah Pemuda Desa (SPD) akan hadir kembali nih,tapi dengan format yang sedikit berbeda dari biasanya.Setelah 4 kali penyelenggaraan di DIY dan Jawa Tengah,akhir tahun ini SPD akan diselenggarakan di 3 kabupaten di 3 provinsi sekaligus."
         },
         {
-            title: "Sekolah Pemuda Desa",
+            title: "Anti Corruption Youth Camp",
             desc: "Sekolah Pemuda Desa (SPD) akan hadir kembali nih,tapi dengan format yang sedikit berbeda dari biasanya.Setelah 4 kali penyelenggaraan di DIY dan Jawa Tengah,akhir tahun ini SPD akan diselenggarakan di 3 kabupaten di 3 provinsi sekaligus."
         }
     ]
     const router = useRouter()
     return (
         <>
+            <div className="p-2 md:p-8 bg-[#F5F5F5] ">
+                <div className="md:ml-14">
+                    <h1 className="text-2xl md:text-4xl ">Program</h1>
+                    <p className="">Berikut adalah program unggulan dari Ketjil Bergerak!</p>
+                </div>
             {program.map((list, index) => (
-                <div key={list.title} className={`p-2 py-8 text-white md:flex gap-10 justify-evenly items-center mx-auto flex-wrap ${index === 1 || index === 3 ? "md:flex-row-reverse" : ""}`}>
+                <div key={list.title} className={`p-2 py-8 bg-[#F5F5F5] text-white md:flex gap-10 justify-evenly items-center mx-auto flex-wrap ${index === 1 || index === 3 ? "md:flex-row-reverse" : ""}`}>
                     <div>
-                        <Image src={kb4} width={450} height={500} alt='' className="" />
+                        <Image src={kb4} width={450} height={500} alt='' className="md:w-[350px]" />
                     </div>
                     <div>
                         <h1 className="mt-2 text-[#F07167] text-2xl md:text-4xl">{list.title}</h1>
@@ -41,6 +46,7 @@ export const ProgramSection = () => {
                     </div>
                 </div>
             ))}
+            </div>
         </>
     )
 };
