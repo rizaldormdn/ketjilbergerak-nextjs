e4daa8a793d4c69b57c773f72d52d975d27231f4
faizkur <mfaizk26@gmail.com>
2023-03-07 13:52:23 +0700

    perubahan

--- src/components/molecules/CardContent.tsx
+++ src/components/molecules/CardContent.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
-import kb3 from "../../assets/images/kb3.png";
-import kb2 from "../../assets/images/kb2.png";
+import kb2 from "../../assets/images/KB2.png";
+import kb3 from "../../assets/images/KB3.png";
+import kb4 from "../../assets/images/KB4.png";
 
 
 const CardContent = () => {
   const listContent = [
     {
-      image: kb3,
+      image: kb2,
       title: "Dapoer Bergerak",
       desc: "Lorem ipsum dolor sit amet consectet adipiscing elit eget quamumto.",
     },
     {
-      image: kb3,
+      image: kb2,
       title: "Pemuda Desa",
       desc: "Lorem ipsum dolor sit amet consectet adipiscing elit eget quamumto.",
     },
     {
-      image: kb3,
+      image: kb2,
       title: "Company growth",
       desc: "Lorem ipsum dolor sit amet consectet adipiscing elit eget quamumto.",
     }
@@ -25,7 +26,7 @@ const CardContent = () => {
     <>
     {listContent.map((list) => (
       <div key={list.title} className="relative">
-      <Image className="rounded-xl opacity-80" src={list.image} width={300} height={225} alt='' />
+      <Image className="rounded-xl opacity-80" src={list.image} width={300} height={175} alt='' />
       <div className="absolute inset-0 flex flex-col justify-end p-4">
         <h1 className="text-[#F07167]">{list.title}</h1>
         <p className="text-white">{list.desc}</p>
---
+++ src/pages/blogs/ReadMore.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ReadMore() {
+  return (
+    <div>read-more</div>
+  )
+}
+
+export default ReadMore
