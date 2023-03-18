94d58ef6a736dab8fff85c827b42e5e7a6063f7a
rizaldormdn <rizaldormdn@gmail.com>
2023-03-19 00:15:16 +0700

    feat: pagination

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ["image.unsplash.com", "via.placeholder.com"],
+		domains: ["image.unsplash.com", "via.placeholder.com", "localhost"],
 		remotePatterns: [
       {
         protocol: 'https',
